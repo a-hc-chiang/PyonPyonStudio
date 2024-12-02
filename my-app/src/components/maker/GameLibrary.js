395595f7ee3b6789583014e13e0f2b65a4494757
Angela Chiang <achi2048@students.cs.ubc.ca>
2024-12-01 16:57:42 -0800

    vjkhdxfjkhasdjfk

--- my-app/src/components/maker/GameLibrary.js
+++ my-app/src/components/maker/GameLibrary.js
@@ -2,20 +2,67 @@ import './Maker.css';
 import React, { useState, useEffect } from 'react';
 import TopMenu from '../shared/TopMenu';
 import BottomMenu from './shared/BottomMenu';
-import { useNavigate } from 'react-router-dom'; 
 
 const GameLibrary = () => {
+    const [data, setData] = useState(null);
+    const [grid, setGrid] = useState(null);
+    const [error, setError] = useState(null); 
+    const [loading, setLoading] = useState(); 
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:5000/');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                console.log(result);
+                setData(result);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    },[])
+
+    useEffect(() => {
+        if (data == null) {
+            return; 
+        }
+
+        let libraryList = []; 
+
+        data.forEach(function(i) {
+            <div className='LibraryGridView'>
+                
+
+            </div>
+        })
+        
+        setGrid(libraryList); 
+    }, [data])
+
     return(
     <div className="MakerPage">
       <TopMenu color="#df79ce" />
-      {/* <div className={`LPContent ${fadeIn ? 'fade-in' : ''}`}> */}
         <div className={"MakerPage"}>
-          <div className={"LibraryMenu"}>
-            <p>Library</p>
-            <div className="grid-container">
-              <div>asd</div>
-              <div>asd</div>
-              <div>asd</div>
+          <div className={"LibraryMenu darumadrop-one-regular"}>
+            <p style={{fontSize:100, color: 'black', margin: '2px 50px'}}>Library</p>
+            <div className='grid-container'>
+                <div>
+                    adasd
+                </div>
+                <div>
+                    adasd
+                </div>
+                <div>
+                    adasd
+                </div>
+                
 
             </div>
           </div>

6960486ed21ed32d90b3d34ad5cf7768d6b21fe7
Ji Tong Yin <jiyin@cs.ubc.ca>
2024-12-01 18:36:01 -0800

    library

--- my-app/src/components/maker/GameLibrary.js
+++ my-app/src/components/maker/GameLibrary.js
@@ -4,11 +4,22 @@ import TopMenu from '../shared/TopMenu';
 import BottomMenu from './shared/BottomMenu';
 
 const GameLibrary = () => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([
+      {
+        "game_id": 1, 
+        "game_name": "skibidi in ohio",
+        "game_bg": "./backgrounds/test1.jpg",
+      },
+      {
+        "game_id": 2,
+        "game_name": "bladee and kai cenat pog",
+        "game_bg": "./backgrounds/test2.jpg",
+      }
+    ]);
     const [grid, setGrid] = useState(null);
     const [error, setError] = useState(null); 
     const [loading, setLoading] = useState(); 
-
+    
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -37,10 +48,13 @@ const GameLibrary = () => {
         let libraryList = []; 
 
         data.forEach(function(i) {
-            <div className='LibraryGridView'>
-                
+          console.log(i)
+          libraryList.push(
+            <div style={{ backgroundImage: `url(${i.game_bg})` }} className='LibraryGridView'>
+                <p style={{margin: '2px 8px'}}>{i.game_name}</p>
 
             </div>
+          );
         })
         
         setGrid(libraryList); 
@@ -53,16 +67,7 @@ const GameLibrary = () => {
           <div className={"LibraryMenu darumadrop-one-regular"}>
             <p style={{fontSize:100, color: 'black', margin: '2px 50px'}}>Library</p>
             <div className='grid-container'>
-                <div>
-                    adasd
-                </div>
-                <div>
-                    adasd
-                </div>
-                <div>
-                    adasd
-                </div>
-                
+              {grid}
 
             </div>
           </div>
